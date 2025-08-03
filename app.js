1f6826720e717d47154717f6e73be7c21e66c35c
allanjoseph01 <allanjoseph3005@gmail.com>
2025-08-03 19:17:59 +0530

    Backend setup done

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const path = require('path');
+
+
+app.set('view engine' , 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname,"public")));
+
+
+app.get('/',function(req,res){
+  res.render('index');
+})
+
+app.listen(3000);
